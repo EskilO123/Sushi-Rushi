9bfe41adc7ed05d0145b67912d00fdd928313326
Isam0506 <Isamsadek1114@gmail.com>
2026-01-28 15:25:17 +0100

    Added interactionScript
    Added interaction script with a grab code inside which allows you to grab objects.

--- Sushi rushi/Assets/Scripts/InteractionScript.cs
+++ Sushi rushi/Assets/Scripts/InteractionScript.cs
@@ -2,27 +2,46 @@ using UnityEngine;
 
 public class InteractionScript : MonoBehaviour
 {
-    bool Haspickup = false;
-    [SerializeField] float pickUpDelay = 0.1f;
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private Transform rayPoint;
+    [SerializeField] private float rayDistance;
+   
+    private GameObject grabbedObject;
+
+    private int layerIndex;
     void Start()
     {
-        
+        layerIndex = LayerMask.NameToLayer("Objects");
     }
 
     
     void Update()
     {
-        
+        CollisionDetector();
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+   private void CollisionDetector()
     {
-        if(other.CompareTag("Pickup") && !Haspickup)
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            Haspickup = true;
-            Destroy(other.gameObject, pickUpDelay);
-            Debug.Log("PickedUp");
+            grabbedObject = hitInfo.collider.gameObject;
+            grabbedObject.GetComponent<Rigidbody2D>();
+            grabbedObject.transform.position = grabPoint.position;
+            grabbedObject.transform.SetParent(transform);
+
         }
+
+        else if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            grabbedObject.GetComponent<Rigidbody2D>();
+            grabbedObject.transform.SetParent(null);
+            grabbedObject = null;
+        }
+
+        Debug.DrawRay(rayPoint.position, transform.right * rayDistance);
+        
     }
 
 }
