cef29ce24ba2b11edb25146b2cf62d6d81f5d9dd
JihadRashdan <jehadrashdan123@gmail.com>
2026-01-28 16:03:58 +0100

    Updated interaction script
    Unfinished

--- Sushi rushi/Assets/Scripts/InteractionScript.cs
+++ Sushi rushi/Assets/Scripts/InteractionScript.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InteractionScript : MonoBehaviour
 {
     [SerializeField] private Transform grabPoint;
     [SerializeField] private Transform rayPoint;
     [SerializeField] private float rayDistance;
+    
    
     private GameObject grabbedObject;
 
     private int layerIndex;
+
+    InputAction interactionAction;
+
+    Rigidbody2D rb;
     void Start()
     {
         layerIndex = LayerMask.NameToLayer("Objects");
+        interactionAction = InputSystem.actions.FindAction("Interact");
+
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     
@@ -24,16 +34,16 @@ public class InteractionScript : MonoBehaviour
     {
         RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
 
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (interactionAction.IsPressed())
         {
             grabbedObject = hitInfo.collider.gameObject;
-            grabbedObject.GetComponent<Rigidbody2D>();
+            grabbedObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
             grabbedObject.transform.position = grabPoint.position;
             grabbedObject.transform.SetParent(transform);
 
         }
 
-        else if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        else if (interactionAction.IsPressed())
         {
             grabbedObject.GetComponent<Rigidbody2D>();
             grabbedObject.transform.SetParent(null);
