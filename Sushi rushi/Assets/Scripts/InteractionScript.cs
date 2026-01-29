87b282738e22ee08dd712e75896fc2a85caf8188
Isam0506 <Isamsadek1114@gmail.com>
2026-01-29 14:57:21 +0100

    Updated Interaction script a bit

--- Sushi rushi/Assets/Scripts/InteractionScript.cs
+++ Sushi rushi/Assets/Scripts/InteractionScript.cs
@@ -1,14 +1,16 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InteractionScript : MonoBehaviour
 {
+    
+
     [SerializeField] private Transform grabPoint;
     [SerializeField] private Transform rayPoint;
     [SerializeField] private float rayDistance;
-    
    
-    private GameObject grabbedObject;
+    public GameObject Boxitem;
 
     private int layerIndex;
     bool isHolding = false;
@@ -18,7 +20,7 @@ public class InteractionScript : MonoBehaviour
     Rigidbody2D rb;
     void Start()
     {
-        layerIndex = LayerMask.NameToLayer("Objects");
+        layerIndex = LayerMask.NameToLayer("Box");
         interactionAction = InputSystem.actions.FindAction("Interact");
         rb = GetComponent<Rigidbody2D>();
     }
@@ -33,29 +35,29 @@ public class InteractionScript : MonoBehaviour
    
     private void CollisionDetector()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.up, rayDistance);
 
         if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
         {
             if (interactionAction.WasPressedThisFrame() && isHolding == false)
             {
 
-                grabbedObject = hitInfo.collider.gameObject;
-                grabbedObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-                grabbedObject.transform.position = grabPoint.position;
-                grabbedObject.transform.SetParent(transform);
+                Boxitem = hitInfo.collider.gameObject;
+                Boxitem.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                Instantiate(Boxitem, grabPoint.position,Quaternion.identity);
+                Boxitem.transform.SetParent(transform);
                 isHolding = true;
 
             }
             else if (interactionAction.WasPressedThisFrame())
             {
-                grabbedObject.transform.SetParent(null);
-                grabbedObject = null;
+                Boxitem.transform.SetParent(null);
+                Boxitem = null;
                 isHolding = false;
             }
         }
 
-        Debug.DrawRay(rayPoint.position, transform.right * rayDistance);
+        Debug.DrawRay(rayPoint.position, transform.up * rayDistance);
         
     }
 
